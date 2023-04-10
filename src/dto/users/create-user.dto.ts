87e772df634046a6c5c543429cc6c787c0723352
Gustavo <gustavo@pop-os.localdomain>
2023-04-10 19:59:05 -0300

    pre merge

--- src/dto/users/create-user.dto.ts
+++ src/dto/users/create-user.dto.ts
@@ -1,14 +1,38 @@
+import { IsBoolean, IsString, IsInt, IsEmail} from 'class-validator';
+import internal from 'stream';
+
 export class CreateUserDto {
+
+    @IsEmail()
     email: string;
+
+    @IsString()
     password: string;
+
+    @IsString()
     first_name: string;
+
+    @IsString()
     last_name: string;
+
+    @IsBoolean()
     activated: boolean;
+
+    @IsString()
     forbidden: boolean;
+
+    @IsString()
+    deleted_at: string
+
+    @IsString()
+    updated_at: string
+
+    @IsString()
     remember_token: string;
+
+    @IsString()
     token_profile: string;
-    language: string;
-    genero: string;
-    checked_term: boolean;
+
+    @IsString()
     last_login_at: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './http/modules/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('Documentação das Endpoints')
+    .setVersion('1.0')
+    .addTag('Usuários')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
