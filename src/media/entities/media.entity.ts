38824a0d8484debd73e64d90fd2f268346219a9a
Matheus Antonio <matheus.santos@mottu.com.br>
2023-04-23 16:41:42 -0300

    [feat] - creating cruds

--- src/media/entities/media.entity.ts
+++ src/media/entities/media.entity.ts
@@ -1,1 +1,30 @@
-export class Media {}
+import {
+  Entity,
+  Column,
+  OneToMany,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from "typeorm";
+
+export class Media {
+  @PrimaryGeneratedColumn({ type: "bigint", unsigned: true })
+  id: number;
+
+  @Column({ length: 256 })
+  media_type: string;
+
+  @Column({ length: 256 })
+  filename: string;
+
+  @CreateDateColumn({ type: "timestamp", nullable: true })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: "timestamp", nullable: true })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ type: "timestamp", nullable: true })
+  deletedAt: Date;
+}
