98bb84ab6e88e363b6b2befe54ae001bfb189e97
Matheus Antonio <matheus.santos@mottu.com.br>
2023-05-20 19:33:06 -0300

    feat: deleteFile implemented

--- src/s3/s3Bucket.service.ts
+++ src/s3/s3Bucket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
-import { S3 } from "aws-sdk";
+import { AWSError, S3 } from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 @Injectable()
 export class UploadS3Service {
@@ -33,32 +34,32 @@ export class UploadS3Service {
     return result;
   }
 
-  // async getFileOffBucket(
-  //   keyOfFile: string,
-  //   dataBuffer: Buffer,
-  //   bucketName: string
-  // ) {
-  //   const s3 = new S3();
+  async deleteFileInS3Bucket(
+    keyOfFile: string,
+    bucketName: string
+  ): Promise<PromiseResult<S3.DeleteObjectOutput, AWSError>> {
+    const s3 = new S3();
 
-  //   s3.config.update({
-  //     region: process.env.AWS_REGION,
-  //     credentials: {
-  //       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  //       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  //     },
-  //   });
+    s3.config.update({
+      region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
 
-  //   console.log(s3.config.credentials.accessKeyId);
-  //   console.log(s3.config.credentials.secretAccessKey);
+    console.log(s3.config.credentials.accessKeyId);
+    console.log(s3.config.credentials.secretAccessKey);
 
-  //   const result = await s3
-  //     .get ({
-  //       Bucket: bucketName,
-  //       Body: dataBuffer,
-  //       Key: keyOfFile,
-  //     })
-  //     .promise();
+    const result = await s3
+      .deleteObject({
+        Bucket: bucketName,
+        Key: keyOfFile,
+      })
+      .promise();
 
-  //   console.log(result);
-  // }
+    console.log(result);
+
+    return result;
+  }
 }
